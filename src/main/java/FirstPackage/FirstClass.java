8ed282be070ef30a6d62b8d7d3691d30603c8bae
apsgithub <ravindran.aparna@gmail.com>
2026-02-19 18:52:51 +0530

    added code

--- src/main/java/FirstPackage/FirstClass.java
+++ src/main/java/FirstPackage/FirstClass.java
@@ -1,9 +1,17 @@
 package FirstPackage;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.WebDriver;
 
 public class FirstClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
 
 	}
 
