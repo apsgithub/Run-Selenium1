37825dcc460a3b7477c510ac74f9ed5ea9640e70
apsgithub <ravindran.aparna@gmail.com>
2026-02-19 19:03:38 +0530

    testng code added

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import FirstPackage.FirstClass;
+
+public class NewTest {
+  @Test
+  public void Test1()
+  
+  {
+	  
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+  }
+}
